76bde6ea229296bb8edb4ca18c95fe3c514d0a55
bieniucieniu <mikibien@outlook.com>
2023-06-05 12:48:44 +0200

    add scene

---
+++ components/Scene.tsx
@@ -0,0 +1,32 @@
+"use clien";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { Mesh } from "three";
+
+function Capsule() {
+  const ref = useRef<Mesh>(null!);
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.x += 0.01;
+      ref.current.rotation.y += 0.01;
+      ref.current.rotation.z += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={ref} position={[0, 0, 0]}>
+      <capsuleGeometry args={[5, 5, 10, 20]} />
+      <meshNormalMaterial />
+    </mesh>
+  );
+}
+
+export default function Scene() {
+  return (
+    <Canvas camera={{ position: [0, 0, 20] }}>
+      <ambientLight />
+      <Capsule />
+    </Canvas>
+  );
+}
