7232ed23ae5520e9c6f2fc0024ec5cadead1575b
bieniucieniu <mikibien@outlook.com>
2023-06-05 16:15:29 +0200

    add styling and links

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Scene from "@/components/Scene";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <Scene />
+    <main className="min-h-screen flex flex-col bg-black text-white">
+      <Scene className="absolute top-0 left-0 h-screen w-screen overflow-hidden" />
+      <nav className="flex flex-row justify-around text-3xl underline">
+        <a target="_blank" href="https://github.com/bieniucieniu">
+          github
+        </a>
+        <Link href="/breakout">breakout</Link>
+        <Link href="/sorting">sorting</Link>
+        <Link href="/beakout">beakout (pre breakout)</Link>
+      </nav>
     </main>
   );
 }
