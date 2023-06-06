db74d7a5a982bb687d78d480cbb5836e7ae61458
bieniucieniu <mikibien@outlook.com>
2023-06-06 22:00:01 +0200

    make nav and bg fixed fix fots

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,34 +3,37 @@ import "./globals.css";
 import { JetBrains_Mono } from "next/font/google";
 import Link from "next/link";
 
-const jetBrainsMono = JetBrains_Mono({ weight: ["300", "400", "600"] });
+const jetBrainsMono = JetBrains_Mono({
+	weight: ["300", "400", "600"],
+	subsets: ["latin"],
+});
 
 export const metadata = {
-  title: "i'm broke",
-  description: "yes i am",
+	title: "i'm broke",
+	description: "yes i am",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={jetBrainsMono.className}>
-      <Scene className="absolute top-0 left-0 h-screen w-screen overflow-hidden -z-10 bg-black" />
-	  <div className="min-h-screen flex flex-col text-white m-0 p-0">
-      <nav className="flex flex-row justify-around text-3xl underline">
-        <a target="_blank" href="https://github.com/bieniucieniu">
-          github
-        </a>
-        <Link href="/breakout">breakout</Link>
-        <Link href="/sorting">sorting</Link>
-        <Link href="/beakout">beakout (pre breakout)</Link>
-      </nav>
-        {children}
-	  </div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={jetBrainsMono.className}>
+				<Scene className="fixed h-screen w-screen overflow-hidden -z-10" />
+				<div className="min-h-screen flex flex-col text-white m-0 p-0">
+					<nav className="fixed flex flex-row justify-around text-2xl underline w-screen">
+						<a target="_blank" href="https://github.com/bieniucieniu">
+							github
+						</a>
+						<Link href="/preview/breakout">breakout</Link>
+						<Link href="/preview/sorting">sorting</Link>
+						<Link href="/preview/beakout">beakout (pre breakout)</Link>
+					</nav>
+					{children}
+				</div>
+			</body>
+		</html>
+	);
 }
