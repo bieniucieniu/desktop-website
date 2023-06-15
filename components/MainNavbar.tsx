5e8ba7f7bfbe282aee41340be3fb03b1a4a5b8f6
bieniucieniu <mikibien@outlook.com>
2023-06-15 21:13:41 +0200

    add tw merge

--- components/MainNavbar.tsx
+++ components/MainNavbar.tsx
@@ -1,35 +1,36 @@
 "use client";
 import Link from "next/link";
+import { twMerge } from "tailwind-merge";
 
 const tabs: { link: string; title: string; targetBlank?: boolean }[] = [
-	{ link: "/", title: "Home" },
-	{ link: "/projects", title: "Projects" },
-	{ link: "github.com/bieniucieniu", title: "Github", targetBlank: true },
+  { link: "/", title: "Home" },
+  { link: "/projects", title: "Projects" },
+  { link: "github.com/bieniucieniu", title: "Github", targetBlank: true },
 ];
 
-export default function MainNavbar(props: React.HTMLProps<HTMLDivElement>) {
-	return (
-		<nav className="stick flex flex-row justify-around text-2xl underline w-screen z-10">
-			{tabs.map((tab) => {
-				if (tab.targetBlank) {
-					return (
-						<Link
-							key={tab.title}
-							href={tab.link}
-							passHref={true}
-							target="_blank"
-						>
-							{tab.title}
-						</Link>
-					);
-				} else {
-					return (
-						<Link key={tab.title} href={tab.link} passHref={true}>
-							{tab.title}
-						</Link>
-					);
-				}
-			})}
-		</nav>
-	);
+export default function MainNavbar({ className }: { className: string }) {
+  return (
+    <nav className={twMerge("stick flex flex-row justify-around text-2xl underline w-screen z-10", className)}>
+      {tabs.map((tab) => {
+        if (tab.targetBlank) {
+          return (
+            <Link
+              key={tab.title}
+              href={tab.link}
+              passHref={true}
+              target="_blank"
+            >
+              {tab.title}
+            </Link>
+          );
+        } else {
+          return (
+            <Link key={tab.title} href={tab.link} passHref={true}>
+              {tab.title}
+            </Link>
+          );
+        }
+      })}
+    </nav>
+  );
 }
