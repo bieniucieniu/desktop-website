d72d866b4de65757ec5d61441ffd92cb75148c07
bieniucieniu <mikibien@outlook.com>
2023-07-06 22:05:37 +0200

    cleanup classes

--- components/ui/Block.tsx
+++ components/ui/Block.tsx
@@ -1,40 +1,42 @@
-import { ReactNode } from "react"
+import type { HTMLProps, ReactNode } from "react"
 import { twMerge } from "tailwind-merge"
 
 export default function Block({
   children,
   className,
   legend,
+  ...props
 }: {
   children?: ReactNode
   className?: string
   legend?: string | [string, string]
-}) {
+} & HTMLProps<HTMLFieldSetElement>) {
   return (
     <fieldset
-      className={twMerge(`border-2 p-1 m-2 flex-1 relative`, className)}
-      style={{ borderBottom: Array.isArray(legend) ? "none" : "" }}
+      {...props}
+      className={twMerge(
+        `border-2 p-1 m-2 flex-1 relative ${
+          Array.isArray(legend) ? "border-b-0" : ""
+        }`,
+        className
+      )}
     >
       {legend ? (
         Array.isArray(legend) ? (
           <>
             <fieldset
-              className="absolute inset-0 rotate-180"
-              style={{ borderTop: "solid 2px" }}
+              {...props}
+              className="absolute inset-0 rotate-180 border-none border-t-2"
             >
-              <legend className="m-0 p-0 rotate-180 " style={{ lineHeight: 0 }}>
+              <legend className="m-0 p-0 leading-[0] rotate-180 mr-4">
                 {legend[1]}
               </legend>
             </fieldset>
 
-            <legend className="m-0 p-0 " style={{ lineHeight: 0 }}>
-              {legend[0]}
-            </legend>
+            <legend className=" leading-[0]">{legend[0]}</legend>
           </>
         ) : (
-          <legend className="m-0 p-0 " style={{ lineHeight: 0 }}>
-            {legend}
-          </legend>
+          <legend className=" leading-[0]">{legend}</legend>
         )
       ) : null}
 
