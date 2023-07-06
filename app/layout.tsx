4bb9be8437250e6a57fd24a6a9f4bec2e45d8234
bieniucieniu <mikibien@outlook.com>
2023-07-06 18:51:23 +0200

    i changed my mind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,21 @@
-import Scene from "@/components/Scene";
-import "./globals.css";
-import { JetBrains_Mono } from "next/font/google";
+import "./globals.css"
+import { JetBrains_Mono } from "next/font/google"
 
-const jetBrainsMono = JetBrains_Mono({ weight: ["300", "400", "600"] });
+const jetBrainsMono = JetBrains_Mono({ weight: ["300", "400", "600"] })
 
 export const metadata = {
-  title: "i'm broke",
-  description: "yes i am",
-};
+	title: "i'm broke",
+	description: "yes i am",
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={jetBrainsMono.className}>
-        <Scene className="absolute top-0 left-0 h-screen w-screen overflow-hidden -z-10 bg-black" />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={jetBrainsMono.className}>{children}</body>
+		</html>
+	)
 }
