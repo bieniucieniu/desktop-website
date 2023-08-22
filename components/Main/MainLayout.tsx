b3d3edda10e62973155a9b2371cf0f0a6a117074
bieniucieniu <mikibien@outlook.com>
2023-08-22 20:25:10 +0200

    add window provider

--- components/Main/MainLayout.tsx
+++ components/Main/MainLayout.tsx
@@ -1,10 +1,13 @@
 import { Taskbar } from "./Taskbar"
+import { WindowProvider, Window } from "@/components/ui/Window"
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="grid grid-rows-[1fr_38px] h-screen">
-      <div className="bg-red-200">{children}</div>
-      <Taskbar />
-    </main>
+    <WindowProvider>
+      <main className="grid grid-rows-[1fr_38px] h-screen">
+        <div className="bg-teal-400 relative overflow-hidden">{children}</div>
+        <Taskbar></Taskbar>
+      </main>
+    </WindowProvider>
   )
 }
