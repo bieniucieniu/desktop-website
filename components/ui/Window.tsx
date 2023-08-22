19ef659c96e300a74ecef817629fd3e2c53fa800
bieniucieniu <mikibien@outlook.com>
2023-08-22 20:24:44 +0200

    change window to framer motion with drag

--- components/ui/Window.tsx
+++ components/ui/Window.tsx
@@ -1,23 +1,29 @@
 "use client"
 import { twMerge } from "tailwind-merge"
-import { HTMLMotionProps, motion } from "framer-motion"
+import { motion, useDragControls } from "framer-motion"
 import { Button } from "./Button"
 import React, {
   createContext,
-  forwardRef,
   useContext,
+  useEffect,
   useId,
+  useMemo,
+  useRef,
   useState,
 } from "react"
-import * as Dialog from "@radix-ui/react-dialog"
+
+type WindowsMap = Map<
+  string,
+  {
+    open: boolean
+    layoutId: string
+    fullScreen: boolean
+  }
+>
 
 type WindowContext = {
-  open: boolean | undefined
-  setOpen: React.Dispatch<React.SetStateAction<boolean | undefined>>
-  layoutId: string
-  fullScreen: boolean
-
-  setFullScreen: React.Dispatch<React.SetStateAction<boolean>>
+  windows: WindowsMap
+  setWindows: React.Dispatch<React.SetStateAction<WindowsMap>>
 }
 
 const WindowContext = createContext<WindowContext | null>(null)
@@ -32,58 +38,85 @@ function useWindowContext() {
   return context
 }
 
-export function Root({ children }: { children: React.ReactNode }) {
-  const [open, setOpen] = useState<boolean | undefined>(undefined)
-  const layoutId = useId()
-  const [fullScreen, setFullScreen] = useState<boolean>(false)
+export function WindowProvider({ children }: { children: React.ReactNode }) {
+  const [windows, setWindows] = useState(new Map())
+
   return (
-    <Dialog.Root open={open}>
-      <WindowContext.Provider
-        value={{ open, setOpen, layoutId, fullScreen, setFullScreen }}
-      >
-        {children}
-      </WindowContext.Provider>
-    </Dialog.Root>
+    <WindowContext.Provider value={{ windows, setWindows }}>
+      {children}
+    </WindowContext.Provider>
   )
 }
 
-export const Content = forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ children, className }, ref) => {
-  const { layoutId } = useWindowContext()
-  return (
-    <Dialog.Portal>
-      <Dialog.Content asChild>
-        <motion.div
-          layoutId={layoutId}
-          ref={ref}
-          className={twMerge(
-            "min-w-[200px] min-h-[200px] border-outset grid grid-cols-1 grid-rows-[20px_repeat(auto-fit,_minmax(200px,_1fr))]",
-            className
-          )}
-        >
-          {children}
-        </motion.div>
-      </Dialog.Content>
-    </Dialog.Portal>
-  )
-})
+export function Window({
+  children,
+  className,
+  windowName,
+}: React.HTMLAttributes<HTMLDivElement> & { windowName?: React.ReactNode }) {
+  const dragControlls = useDragControls()
+  const { windows, setWindows } = useWindowContext()
+  const id = useId()
+  const ref = useRef<HTMLDivElement>(null)
 
-export const Navbar = forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ children, className }, ref) => {
-  const { fullScreen, setOpen, setFullScreen } = useWindowContext()
+  useEffect(() => {
+    if (windows.has(id)) throw new Error("window of given id exist")
+
+    setWindows(
+      new Map(
+        windows.set(id, {
+          open: true,
+          layoutId: id,
+          fullScreen: false,
+        })
+      )
+    )
+    return () => {
+      const s = windows.delete(id)
+      if (s) setWindows(new Map(windows))
+      else console.log("no window with given id")
+    }
+  }, [])
+
+  const { right, bottom } = useMemo(() => {
+    if (ref.current)
+      return {
+        right: window.innerWidth - ref.current.clientWidth,
+        bottom: window.innerHeight - ref.current.clientHeight,
+      }
+    else
+      return { right: window.innerWidth - 400, bottom: window.innerWidth - 400 }
+  }, [ref.current])
+
   return (
-    <Dialog.Title asChild>
+    <motion.div
+      ref={ref}
+      drag
+      dragControls={dragControlls}
+      dragListener={false}
+      dragMomentum={false}
+      dragElastic={0}
+      dragConstraints={{
+        top: 0,
+        left: 0,
+        right,
+        bottom,
+      }}
+      transition={{ ease: "linear" }}
+      style={{ position: "absolute", minWidth: "400px", x: "50%", y: "50%" }}
+      className="border-outset border-2 border-zinc-300 select-none"
+    >
       <nav
-        ref={ref}
-        className={twMerge("flex flex-row justify-between", className)}
+        className={twMerge(
+          "flex flex-row justify-between items-center w-full bg-gray-300",
+          className
+        )}
+        onPointerDown={(e) => dragControlls.start(e)}
       >
-        <div>{children}</div>
-        <section>
-          <Button onClick={() => setOpen((o) => !o)}>
+        <h3 className="text-zinc-700 pl-2 text-xl font-bold">
+          {windowName ?? "new window"}
+        </h3>
+        <section className="flex gap-x-1 pr-1.5">
+          <Button className="border-2 border-outset">
             <svg
               className="h-4 w-4"
               fill="none"
@@ -93,27 +126,21 @@ export const Navbar = forwardRef<
               <path d="M2 3h20v18H2V3zm18 16V7H4v12h16z" fill="currentColor" />
             </svg>
           </Button>
-          <Button onClick={() => setFullScreen((o) => !o)}>
+          <Button className="border-2 border-outset">
             <svg
               className="h-4 w-4"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
             >
-              {fullScreen ? (
-                <path
-                  d="M2 4h20v16H2V4zm18 14V6H4v12h16z"
-                  fill="currentColor"
-                />
-              ) : (
-                <path
-                  d="M2 4h20v16H2V4zm2 14h16V6H4v12zM8 8h2v2H8v2H6V8h2zm8 8h-2v-2h2v-2h2v4h-2z"
-                  fill="currentColor"
-                />
-              )}
+              <path d="M2 4h20v16H2V4zm18 14V6H4v12h16z" fill="currentColor" />
+              <path
+                d="M2 4h20v16H2V4zm2 14h16V6H4v12zM8 8h2v2H8v2H6V8h2zm8 8h-2v-2h2v-2h2v4h-2z"
+                fill="currentColor"
+              />
             </svg>
           </Button>
-          <Button onClick={() => setOpen(undefined)}>
+          <Button className="border-2 border-outset">
             <svg
               className="h-4 w-4"
               fill="none"
@@ -128,35 +155,7 @@ export const Navbar = forwardRef<
           </Button>
         </section>
       </nav>
-    </Dialog.Title>
+      <main className="min-h-[300px]">{children}</main>
+    </motion.div>
   )
-})
-
-export const Trigger = forwardRef<HTMLButtonElement, HTMLMotionProps<"button">>(
-  ({ children, className, onClick, ...props }, ref) => {
-    const { open, setOpen, layoutId } = useWindowContext()
-
-    if (open === undefined) return null
-
-    return (
-      <Dialog.Trigger asChild>
-        <motion.button
-          {...props}
-          layoutId={open ? undefined : layoutId}
-          ref={ref}
-          className={twMerge(
-            "border-2 px-2 py-0.5",
-            open ? "border-inset" : "border-outset",
-            className
-          )}
-          onClick={(e) => {
-            setOpen((o) => !o)
-            onClick && onClick(e)
-          }}
-        >
-          {children}
-        </motion.button>
-      </Dialog.Trigger>
-    )
-  }
-)
+}
