5fa7391063bf91ca8fb13ba73f6def0afb559231
bieniucieniu <mikibien@outlook.com>
2023-08-23 15:09:50 +0200

    move Windows to page

--- components/Main/MainLayout.tsx
+++ components/Main/MainLayout.tsx
@@ -1,12 +1,12 @@
 import { Taskbar } from "./Taskbar"
-import { WindowProvider, Window } from "@/components/ui/Window"
+import { WindowProvider } from "@/components/ui/Window"
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <WindowProvider>
       <main className="grid grid-rows-[1fr_38px] h-screen">
         <div className="bg-teal-400 relative overflow-hidden">{children}</div>
-        <Taskbar></Taskbar>
+        <Taskbar />
       </main>
     </WindowProvider>
   )
