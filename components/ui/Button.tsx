a9b1b1893f2d00a3cf2065beb536b44b9238f8b8
bieniucieniu <mikibien@outlook.com>
2023-08-25 00:52:28 +0200

    update to motion button and anchor

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,37 +1,39 @@
 import { forwardRef } from "react"
 import { twMerge } from "tailwind-merge"
 import NextLink from "next/link"
+import { HTMLMotionProps, motion } from "framer-motion"
 
-const Button = forwardRef<
-  HTMLButtonElement,
-  React.ButtonHTMLAttributes<HTMLButtonElement>
->(({ className, ...props }, ref) => {
-  return (
-    <button
-      className={twMerge(
-        "active-border-button border-2 border-solid bg-zinc-300 text-black px-1 select-none border-zinc-300",
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
+const Button = forwardRef<HTMLButtonElement, HTMLMotionProps<"button">>(
+  ({ className, ...props }, ref) => {
+    return (
+      <motion.button
+        className={twMerge(
+          "active-border-button border-2 border-solid bg-zinc-300 text-black px-1 select-none border-zinc-300",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
 Button.displayName = "Button"
 
 const Link = forwardRef<
   HTMLAnchorElement,
-  React.HTMLAttributes<HTMLAnchorElement> & { href: string }
->(({ className, ...props }, ref) => {
+  HTMLMotionProps<"a"> & { href: string }
+>(({ className, href, ...props }, ref) => {
   return (
-    <NextLink
-      className={twMerge(
-        "active-border-button border-2 border-solid bg-zinc-300 text-black px-1 select-none border-zinc-300",
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
+    <NextLink href={href} legacyBehavior>
+      <motion.a
+        className={twMerge(
+          "active-border-button border-2 border-solid bg-zinc-300 text-black px-1 select-none border-zinc-300",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    </NextLink>
   )
 })
 Link.displayName = "Link"
