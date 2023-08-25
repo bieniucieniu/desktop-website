497d07aa92f289039650604e78414aea6e8967b3
bieniucieniu <mikibien@outlook.com>
2023-08-25 15:07:50 +0200

    add MainContextProvider

--- components/Main/MainLayout.tsx
+++ components/Main/MainLayout.tsx
@@ -1,15 +1,20 @@
+import { MainContextProvider } from "./MainContext"
 import { Taskbar } from "./Taskbar"
 import { WindowProvider, WindowsContainer } from "@/components/ui/Window"
+import WindowRenderer from "./WindowRenderer"
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <WindowProvider>
-      <main className="grid grid-rows-[1fr_38px] h-screen">
-        <WindowsContainer className="bg-teal-400 relative overflow-hidden">
-          {children}
-        </WindowsContainer>
-        <Taskbar />
-      </main>
+      <MainContextProvider>
+        <main className="grid grid-rows-[1fr_38px] h-screen">
+          <WindowsContainer className="bg-teal-400 relative overflow-hidden">
+            {children}
+            <WindowRenderer />
+          </WindowsContainer>
+          <Taskbar />
+        </main>
+      </MainContextProvider>
     </WindowProvider>
   )
 }
