b0776e12e0cdd70c9949dbeb688786a939842e2e
bieniucieniu <mikibien@outlook.com>
2023-08-25 16:25:20 +0200

    change to null

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import { AddWindow } from "@/components/Main/WindowRenderer"
-
 export default function Home() {
-  return (
-    <>
-      <AddWindow name="home" id="home">
-        <div className="text-9xl bg-red-400">adhlksajdhaksljdha</div>
-      </AddWindow>
-    </>
-  )
+  return null
 }
