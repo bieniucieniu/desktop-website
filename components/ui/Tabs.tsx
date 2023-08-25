94f5a63209a9c26286df3d62c331ded299e994cd
bieniucieniu <mikibien@outlook.com>
2023-08-25 18:23:08 +0200

    add styles

--- components/ui/Tabs.tsx
+++ components/ui/Tabs.tsx
@@ -1,25 +1,52 @@
 "use client"
 
 import { Root, List, Trigger, Content } from "@radix-ui/react-tabs"
-import { twMerge } from "tailwind-merge"
+import { twJoin, twMerge } from "tailwind-merge"
 import { Button } from "./Button"
 export function Tabs({ className, ...props }: Parameters<typeof Root>[0]) {
-  return <Root className={twMerge("flex flex-col", className)} {...props} />
+  return (
+    <Root
+      className={twMerge("flex flex-col w-full h-full bg-zinc-100", className)}
+      {...props}
+    />
+  )
 }
 export function TabsList({ className, ...props }: Parameters<typeof List>[0]) {
   return (
-    <List className={twMerge("border-outset border-2", className)} {...props} />
+    <List
+      className={twMerge("flex flex-row gap-x-1 pt-1 px-1", className)}
+      {...props}
+    />
   )
 }
 export function TabsTrigger({
   className,
   ...props
 }: Parameters<typeof Trigger>[0]) {
-  return <Trigger className={twMerge("", className)} {...props} />
+  return (
+    <Trigger
+      className={twMerge(
+        "border-outset data-[state=active]:border-b-0",
+        className
+      )}
+      {...props}
+    />
+  )
 }
 export function TabsContent({
   className,
   ...props
 }: Parameters<typeof Content>[0]) {
-  return <Content className={twMerge("", className)} {...props} />
+  return (
+    <Content
+      className={twJoin(
+        "border-outset",
+        twMerge(
+          "mx-1 bg-zinc-300 text-black border-2 border-zinc-300",
+          className
+        )
+      )}
+      {...props}
+    />
+  )
 }
