d5248be58b8987a6113d2695d9b77e2c7090b8de
bieniucieniu <mikibien@outlook.com>
2023-08-25 18:24:01 +0200

    paceholders

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import { AddWindow } from "@/components/Main/WindowRenderer"
+
+export default function () {
+  return (
+    <AddWindow name="about">
+      <div>dashndlakjdhbakldjahdlkjahsdkljhadkljashdalkj</div>
+    </AddWindow>
+  )
+}
