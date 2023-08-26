f5596c9f539ef66935acc9da2385e7c29e26d917
bieniucieniu <mikibien@outlook.com>
2023-08-26 16:09:52 +0200

    formating

--- components/Main/Taskbar.tsx
+++ components/Main/Taskbar.tsx
@@ -8,58 +8,60 @@ import { MButton } from "../ui/MButton"
 import { useTransform } from "framer-motion"
 
 function WindowButton({
-  style,
-  length,
-  layer,
-  ...props
+	style,
+	length,
+	layer,
+	...props
 }: Parameters<typeof MButton>[0] & {
-  layer: { get: () => number } | undefined
-  length: number
+	layer: { get: () => number } | undefined
+	length: number
 }) {
-  const backgroundColor = useTransform(() =>
-    layer?.get() === length ? "aqua" : undefined
-  )
+	const backgroundColor = useTransform(() =>
+		layer?.get() === length ? "aqua" : undefined
+	)
 
-  return (
-    <MButton
-      className="border-outset"
-      {...props}
-      style={{ backgroundColor, ...style }}
-    />
-  )
+	return (
+		<MButton
+			className="border-outset"
+			{...props}
+			style={{ backgroundColor, ...style }}
+		/>
+	)
 }
 
 export function Taskbar({ className }: { className?: string }) {
-  const { WindowsControlls } = useWindows()
-  return (
-    <nav
-      className={twMerge(
-        "flex flex-row justify-between border-taskbar border-2 bg-zinc-300 py-0.5 px-2",
-        className
-      )}
-    >
-      <section className="flex flex-row gap-x-1">
-        <MainMenu className="z-50" />
-        <Separator.Root
-          orientation="vertical"
-          className="border-outset border my-0.5"
-        />
-
-        {WindowsControlls.map((e) => (
-          <WindowButton
-            onClick={() => {
-              e.open?.set(true)
-              e.focusWindow()
-            }}
-            key={e.id}
-            layer={e.layer}
-            length={WindowsControlls.length}
-          >
-            {e.name}
-          </WindowButton>
-        ))}
-      </section>
-      <Clock />
-    </nav>
-  )
+	const { WindowsControlls } = useWindows()
+	return (
+		<nav
+			className={twMerge(
+				"flex flex-row justify-between border-taskbar border-2 bg-zinc-300 py-0.5 px-2",
+				className
+			)}
+		>
+			<section className="flex flex-row gap-x-1">
+				<MainMenu className="z-50" />
+				<Separator.Root
+					orientation="vertical"
+					className="border-outset border my-0.5"
+				/>
+				<ul className="flex flex-col flex-wrap">
+					{WindowsControlls.map((e) => (
+						<li key={e.id}>
+							<WindowButton
+								onClick={() => {
+									e.open?.set(true)
+									e.focusWindow()
+								}}
+								layer={e.layer}
+								length={WindowsControlls.length}
+							>
+								{e.name}
+							</WindowButton>
+						</li>
+					))}
+				</ul>
+			</section>
+			<Clock />
+		</nav>
+	)
 }
