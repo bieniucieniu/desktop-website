5845e42294dd4da6ed05c5779f6da2cc26f7b822
bieniucieniu <mikibien@outlook.com>
2023-08-26 16:33:21 +0200

    add function name

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import { AddWindow } from "@/components/Main/WindowRenderer"
 
-export default function () {
+export default function About() {
   return (
     <AddWindow name="about">
       <div>dashndlakjdhbakldjahdlkjahsdkljhadkljashdalkj</div>
