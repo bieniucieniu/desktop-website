1d2f95d201f04ab35720e7c7dfc8a6c5dfd879be
bieniucieniu <mikibien@outlook.com>
2023-08-26 16:30:24 +0200

    add redirect to root dir after window close

--- components/Main/WindowRenderer.tsx
+++ components/Main/WindowRenderer.tsx
@@ -3,34 +3,39 @@
 import { ReactElement, useEffect } from "react"
 import { useMainContext } from "./MainContext"
 import { Window } from "../ui/Window"
+import { useRouter } from "next/navigation"
 
 export function AddWindow({
-	children,
-	name,
+  children,
+  name,
 }: {
-	children?: ReactElement
-	name: string
+  children?: ReactElement
+  name: string
 }) {
-	const { addWindow: add } = useMainContext()
-	useEffect(() => {
-		const defaultFullScreen = window.innerWidth < 1024
-		const phone = window.innerWidth < 500
-		add(name, { name, children, defaultFullScreen, phone, customId: name })
-		/* eslint-disable */
-	}, [name, children])
-	/* eslint-enable */
-	return null
+  const { addWindow: add } = useMainContext()
+  useEffect(() => {
+    const defaultFullScreen = window.innerWidth < 1024
+    const phone = window.innerWidth < 500
+    add(name, { name, children, defaultFullScreen, phone, customId: name })
+    /* eslint-disable */
+  }, [name, children])
+  /* eslint-enable */
+  return null
 }
 
 export default function WindowRenderer() {
-	const { windows, deleteWindow } = useMainContext()
+  const { windows, deleteWindow } = useMainContext()
+  const router = useRouter()
 
-	return windows.map(({ id, props }) => (
-		<Window
-			{...props}
-			key={id}
-			onClose={() => deleteWindow(id)}
-			customId={id}
-		/>
-	))
+  return windows.map(({ id, props }) => (
+    <Window
+      {...props}
+      key={id}
+      onClose={() => {
+        deleteWindow(id)
+        router.back()
+      }}
+      customId={id}
+    />
+  ))
 }
