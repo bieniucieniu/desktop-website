ff916af6696fc835f38db34e1fe964b606aa7ca3
bieniucieniu <mikibien@outlook.com>
2023-08-26 15:36:31 +0200

    add redirect

--- components/Main/MainContext.tsx
+++ components/Main/MainContext.tsx
@@ -1,67 +1,71 @@
 "use client"
 import { createContext, useContext, useState } from "react"
 import { Window } from "../ui/Window"
+import { redirect } from "next/navigation"
 
 type WindowProps = { props: Parameters<typeof Window>[0]; id: string }
 
 type MainContext = {
-  windows: WindowProps[]
-  addWindow: (id: string, props: Parameters<typeof Window>[0]) => void
-  deleteWindow: (id: string) => void
-  powerOff: () => void
+	windows: WindowProps[]
+	addWindow: (id: string, props: Parameters<typeof Window>[0]) => void
+	deleteWindow: (id: string) => void
+	powerOff: () => void
 }
 
 const MainContext = createContext<MainContext | null>(null)
 
 export function useMainContext() {
-  const context = useContext(MainContext)
+	const context = useContext(MainContext)
 
-  if (!context) {
-    throw new Error("not in window context")
-  }
+	if (!context) {
+		throw new Error("not in window context")
+	}
 
-  return context
+	return context
 }
 
 export function MainContextProvider({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  const [windows, setWindows] = useState<WindowProps[]>([])
-  const [powered, setPowered] = useState<boolean>(true)
+	const [windows, setWindows] = useState<WindowProps[]>([])
+	const [powered, setPowered] = useState<boolean>(true)
 
-  function addWindow(id: string, props: Parameters<typeof Window>[0]) {
-    const win = windows.find((w) => w.id === id)
+	function addWindow(id: string, props: Parameters<typeof Window>[0]) {
+		const win = windows.find((w) => w.id === id)
 
-    if (win) {
-      const newWin = windows.map((e) => {
-        if (e.id === id) {
-          return { id, props }
-        }
-        return e
-      })
-      setWindows(newWin)
-    } else {
-      windows.push({ id, props })
-      setWindows([...windows])
-    }
-  }
+		if (win) {
+			const newWin = windows.map((e) => {
+				if (e.id === id) {
+					return { id, props }
+				}
+				return e
+			})
+			setWindows(newWin)
+		} else {
+			windows.push({ id, props })
+			setWindows([...windows])
+		}
+	}
 
-  function deleteWindow(id: string) {
-    const newWin = windows.filter((w) => w.id !== id)
-    setWindows(newWin)
-  }
-  return (
-    <MainContext.Provider
-      value={{
-        windows,
-        addWindow,
-        deleteWindow,
-        powerOff: () => setPowered(false),
-      }}
-    >
-      {powered ? children : null}
-    </MainContext.Provider>
-  )
+	function deleteWindow(id: string) {
+		const newWin = windows.filter((w) => w.id !== id)
+		setWindows(newWin)
+	}
+	return (
+		<MainContext.Provider
+			value={{
+				windows,
+				addWindow,
+				deleteWindow,
+				powerOff: () => {
+					setPowered(false)
+					redirect("/")
+				},
+			}}
+		>
+			{powered ? children : null}
+		</MainContext.Provider>
+	)
 }
