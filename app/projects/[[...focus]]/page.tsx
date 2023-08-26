0a85c5ed503a55348a8cfe6ab9459b18758c1086
bieniucieniu <mikibien@outlook.com>
2023-08-26 16:19:16 +0200

    formating

--- app/projects/[[...focus]]/page.tsx
+++ app/projects/[[...focus]]/page.tsx
@@ -3,33 +3,33 @@ import { Button } from "@/components/ui/Button"
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/Tabs"
 
 export default function Projects({
-	params,
+  params,
 }: {
-	params: { focus: string[] | undefined }
+  params: { focus: string[] | undefined }
 }) {
-	const { focus } = params
-	return (
-		<AddWindow name="projects">
-			<Tabs defaultValue={focus === undefined ? "about" : focus[0]}>
-				<TabsList>
-					<TabsTrigger value="about" asChild>
-						<Button>About</Button>
-					</TabsTrigger>
-					<TabsTrigger value="braciabien" asChild>
-						<Button>Bracia Bien</Button>
-					</TabsTrigger>
-					<TabsTrigger value="breakout" asChild>
-						<Button>Breakout</Button>
-					</TabsTrigger>
-					<TabsTrigger value="portfolio" asChild>
-						<Button>Portfolio</Button>
-					</TabsTrigger>
-				</TabsList>
-				<TabsContent value="about">about</TabsContent>
-				<TabsContent value="braciabien">braciabien</TabsContent>
-				<TabsContent value="breakout">breakout</TabsContent>
-				<TabsContent value="portfolio">portfolio</TabsContent>
-			</Tabs>
-		</AddWindow>
-	)
+  const { focus } = params
+  return (
+    <AddWindow name="projects">
+      <Tabs defaultValue={focus === undefined ? "about" : focus[0]}>
+        <TabsList>
+          <TabsTrigger value="about" asChild>
+            <Button>About</Button>
+          </TabsTrigger>
+          <TabsTrigger value="braciabien" asChild>
+            <Button>Bracia Bien</Button>
+          </TabsTrigger>
+          <TabsTrigger value="breakout" asChild>
+            <Button>Breakout</Button>
+          </TabsTrigger>
+          <TabsTrigger value="portfolio" asChild>
+            <Button>Portfolio</Button>
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="about">about</TabsContent>
+        <TabsContent value="braciabien">braciabien</TabsContent>
+        <TabsContent value="breakout">breakout</TabsContent>
+        <TabsContent value="portfolio">portfolio</TabsContent>
+      </Tabs>
+    </AddWindow>
+  )
 }
