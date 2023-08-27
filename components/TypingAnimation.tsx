65e87558ce6bcbfc492202803b3185ff5b9f51f9
bieniucieniu <mikibien@outlook.com>
2023-08-27 18:34:16 +0200

    add typing animation

---
+++ components/TypingAnimation.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { useMemo } from "react"
+import { TypeAnimation } from "react-type-animation"
+
+type Props = Parameters<typeof TypingAnimation>[0]
+export default function TypingAnimation({
+  children,
+  className,
+  onComplete,
+  speed,
+}: {
+  children: string | string[]
+  className?: string
+  onComplete?: () => void
+  // @ts-ignore
+  speed?: Props["speed"]
+}) {
+  const seq = useMemo(() => {
+    const arr = Array.isArray(children)
+      ? children.reduce((ac, v) => {
+          ac.push(v, 1000)
+          return ac
+        }, [] as Array<string | number | (() => void)>)
+      : [children]
+    if (onComplete) arr.push(onComplete)
+    return arr
+  }, [children])
+
+  return (
+    <TypeAnimation
+      className={className}
+      cursor={false}
+      sequence={seq}
+      wrapper="p"
+      speed={speed ?? 100}
+    />
+  )
+}
