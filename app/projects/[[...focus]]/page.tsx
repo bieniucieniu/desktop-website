90f6273d4bb405ea6da4d770ba924e877edecf08
bieniucieniu <mikibien@outlook.com>
2023-08-28 17:50:20 +0200

    add description in list

--- app/projects/[[...focus]]/page.tsx
+++ app/projects/[[...focus]]/page.tsx
@@ -1,5 +1,4 @@
 import { AddWindow } from "@/components/Main/WindowRenderer"
-import Link from "next/link"
 import { Button } from "@/components/ui/Button"
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/Tabs"
 
@@ -11,8 +10,11 @@ export default function Projects({
   const { focus } = params
   return (
     <AddWindow name="projects">
-      <Tabs defaultValue={focus !== undefined ? focus[0] : "list"}>
-        <TabsList>
+      <Tabs
+        className="max-w-[100vw]"
+        defaultValue={focus !== undefined ? focus[0] : "list"}
+      >
+        <TabsList className="overflow-x-auto">
           <TabsTrigger value="list" asChild>
             <Button>list</Button>
           </TabsTrigger>
@@ -37,24 +39,76 @@ export default function Projects({
         </TabsList>
         <TabsContent value="list">
           <TabsList asChild>
-            <ul className="flex flex-col">
+            <ul className="flex flex-col gap-y-3 p-2 text-xl max-w-screen-lg">
               <TabsTrigger value="braciabien" asChild>
-                <li className="hover:underline">Bracia Bien</li>
+                <li className="group border-b">
+                  <h1 className="font-bold text-2xl group-hover:underline">
+                    Bracia Bien
+                  </h1>
+                  <p>
+                    Web page for my father&apos;s shop. Build with next.js 13,
+                    shadcn-ui and framer-motion. It has fully dynamic images
+                    stored on bucket and controlled via postgres database on
+                    admin page.
+                  </p>
+                </li>
               </TabsTrigger>
               <TabsTrigger value="breakout" asChild>
-                <li className="hover:underline">Breakout</li>
+                <li className="group border-b">
+                  <h1 className="font-bold text-2xl group-hover:underline">
+                    Breakout
+                  </h1>
+                  <p>
+                    Copy of breakout game from far past, build mainly in vanilla
+                    react and three js. It has &quot;gravity&quot; version but
+                    works terible :/.
+                  </p>
+                </li>
               </TabsTrigger>
               <TabsTrigger value="portfolio" asChild>
-                <li className="hover:underline">Portfolio</li>
+                <li className="group border-b">
+                  <h1 className="font-bold text-2xl group-hover:underline">
+                    Portfolio
+                  </h1>
+                  <p>
+                    This website :). It has &quot;window manager&quot; build
+                    with framer motion, what&apos;s intresting content in window
+                    can be rendered on next.js server.
+                  </p>
+                </li>
               </TabsTrigger>
               <TabsTrigger value="sorting" asChild>
-                <li className="hover:underline">sorting</li>
+                <li className="group border-b">
+                  <h1 className="font-bold text-2xl group-hover:underline">
+                    sorting
+                  </h1>
+                  <p>
+                    My first real project with vanilla js/ts, it uses html
+                    canvas to visualize 4 sorting algorithms.
+                  </p>
+                </li>
               </TabsTrigger>
               <TabsTrigger value="weatherapp" asChild>
-                <li className="hover:underline">weather app in angular</li>
+                <li className="group border-b">
+                  <h1 className="font-bold text-2xl group-hover:underline">
+                    weather app in angular
+                  </h1>
+                  <p>
+                    My only project with angular. It&apos;s use open weather api
+                    to display teperature, weather conditon and wind.
+                  </p>
+                </li>
               </TabsTrigger>
               <TabsTrigger value="other" asChild>
-                <li className="hover:underline">other</li>
+                <li className="group border-b">
+                  <h1 className="font-bold text-2xl group-hover:underline">
+                    other
+                  </h1>
+                  <p>
+                    some project of mine not directly connected with front-end
+                    like discord bot, only css project or my dotfiles.
+                  </p>
+                </li>
               </TabsTrigger>
             </ul>
           </TabsList>
