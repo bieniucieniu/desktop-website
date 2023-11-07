34d8fe3c4c3df6c54df5f1458910af9b2b7ee0cb
bieniucieniu <mikibien@outlook.com>
2023-11-07 12:52:11 +0100

    change quot to html's ones

--- components/Main/Wallpaper.tsx
+++ components/Main/Wallpaper.tsx
@@ -11,8 +11,12 @@ export default function Wallpaper({ className }: { className: string }) {
       )}
     >
       <div className="flex flex-col justify-end lg:justify-start p-10 md:pl-28 md:pb-10 lg:pt-10 lg:pr-10">
-        <h1 className="text-5xl sm:text-7xl md:text-9xl">"Mikołaj Bień"</h1>
-        <p className="text-3xl sm:text-xl xl:pl-20">front-end web developer</p>
+        <h1 className="text-5xl sm:text-7xl md:text-9xl">
+          &quot;Mikołaj Bień&quot;
+        </h1>
+        <p className="text-3xl sm:text-xl xl:pl-20">
+          front-end web developer, from Poland
+        </p>
       </div>
     </div>
   )
