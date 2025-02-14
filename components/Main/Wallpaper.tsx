be3cc3a8445e23dadd97fc5f3606fd1bd665eeee
bieniu <mikibien@outlook.com>
2025-02-14 12:09:40 +0100

    feat: remove text from wallpaper

--- components/Main/Wallpaper.tsx
+++ components/Main/Wallpaper.tsx
@@ -1,23 +1,16 @@
-import { twMerge } from "tailwind-merge"
-import { vast } from "@/fonts"
+import { vast } from "@/fonts";
+import { twMerge } from "tailwind-merge";
 
 export default function Wallpaper({ className }: { className: string }) {
-  return (
-    <div
-      className={twMerge(
-        className,
-        vast.className,
-        "bg-teal-400 flex justify-end",
-      )}
-    >
-      <div className="flex flex-col justify-end lg:justify-start p-10 md:pl-28 md:pb-10 lg:pt-10 lg:pr-10">
-        <h1 className="text-5xl sm:text-7xl md:text-9xl">
-          &quot;Mikołaj Bień&quot;
-        </h1>
-        <p className="text-3xl sm:text-xl xl:pl-20">
-          front-end web developer, from Poland
-        </p>
-      </div>
-    </div>
-  )
+	return (
+		<div
+			className={twMerge(
+				className,
+				vast.className,
+				"bg-teal-400 flex justify-end",
+			)}
+		>
+			<div className="flex flex-col justify-end lg:justify-start p-10 md:pl-28 md:pb-10 lg:pt-10 lg:pr-10" />
+		</div>
+	);
 }
